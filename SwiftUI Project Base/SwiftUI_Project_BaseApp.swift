3c81dfe8bd100343eb7d27ae1fdc9da4ad9dde5e
Abdmin <mukmin@terato.com>
2022-10-04 17:31:35 +0800

    First commit

---
+++ SwiftUI Project Base/SwiftUI_Project_BaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Project_BaseApp.swift
+//  SwiftUI Project Base
+//
+//  Created by Abdmin on 04/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Project_BaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
